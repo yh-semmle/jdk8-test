fab8ccbb03d3d8d5b96ab903b7f79fb80e5bc95b
yh-semmle <43018487+yh-semmle@users.noreply.github.com>
2020-03-20 23:35:40 -0400

    Make commit SHA for `get-source-jdk8.sh` configurable

--- get-source-jdk8.sh
+++ get-source-jdk8.sh
@@ -2,9 +2,11 @@
 
 set -eux
 
+SHA=$1
+
 hg clone http://hg.openjdk.java.net/jdk8u/jdk8u
 cd jdk8u
-hg checkout 3489892a3253 # jdk8u222-b10
+hg checkout $1
 chmod u+x get_source.sh
 ./get_source.sh
 chmod u+x configure
